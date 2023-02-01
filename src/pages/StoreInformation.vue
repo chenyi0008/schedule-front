c687bdd83d9c1c7e643acffeeebffa257acc16a8
xujunjun <904752171@qq.com>
2023-02-01 20:10:05 +0800

    return

--- src/pages/StoreInformation.vue
+++ src/pages/StoreInformation.vue
@@ -1,112 +1,61 @@
 <template>
-  <el-table :data="tableData" style="width: 100%" max-height="850">
-    <el-table-column fixed prop="date" label="日期" width="150">
-    </el-table-column>
-    <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
-    <el-table-column prop="province" label="省份" width="120">
-    </el-table-column>
-    <el-table-column prop="city" label="市区" width="120"> </el-table-column>
-    <el-table-column prop="address" label="地址" width="300"> </el-table-column>
-    <el-table-column prop="zip" label="邮编" width="120"> </el-table-column>
-    <el-table-column fixed="right" label="操作" width="240">
-      <template slot-scope="scope">
-        <el-button @click.native.prevent="addRow()" type="text" size="small">
-          添加
-        </el-button>
-        <el-button
-          @click.native.prevent="deleteRow(scope.$index, tableData)"
-          type="text"
-          size="small"
-        >
-          删除
-        </el-button>
-        <el-button
-          @click.native.prevent="modifyRow(scope.$index, tableData)"
-          type="text"
-          size="small"
-        >
-          修改
-        </el-button>
-        <el-button
-          @click.native.prevent="viewRow(scope.$index, tableData)"
-          type="text"
-          size="small"
-        >
-          查看
-        </el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table
+      ref="multipleTable"
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" width="55"> </el-table-column>
+      <!-- <el-table-column label="商店id" width="120"> -->
+      <!--   <template v-slot="scope">{{ scope.row.id}}</template> -->
+      <!-- </el-table-column> -->
+      <el-table-column prop="name" label="名称" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+      <el-table-column prop="size" label="面积" show-overflow-tooltip></el-table-column>
+    </el-table>
+    <div style="margin-top: 20px">
+      <el-button @click="toggleSelection([tableData[1], tableData[2]])"
+        >切换第二、第三行的选中状态</el-button
+      >
+      <el-button @click="toggleSelection()">取消选择</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import {getAllStore} from '@/apis/store'
 export default {
-  methods: {
-    deleteRow(index, rows) {
-      rows.splice(index, 1);
-    },
-  },
+  name: "tableView",
   data() {
     return {
       tableData: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-        {
-          date: "2016-05-08",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-        {
-          date: "2016-05-06",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
       ],
+      multipleSelection: [],
     };
+  },
+  mounted() {
+    getAllStore().then(res=>{
+      console.log(res.data.data);
+      this.tableData = res.data.data;
+    })
+  },
+  methods: {
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach((row) => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
   },
 };
 </script>
+
+<style lang="less" scoped></style>
