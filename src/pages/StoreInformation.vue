75daf765b3f1e9d1dbf914750745b1842d0d987f
xujunjun <904752171@qq.com>
2023-03-27 00:20:38 +0800

    Add Automatic Refresh

--- src/pages/StoreInformation.vue
+++ src/pages/StoreInformation.vue
@@ -36,8 +36,6 @@
       </el-form>
     </el-dialog>
 
-    
-
     <!-- 对话框 -->
     <el-form
       :inline="true"
@@ -65,19 +63,30 @@
       </el-form-item>
     </el-form>
 
-        <!-- 编辑小组详情模态框 -->
-        <el-dialog :title="infoDialogTitle" :visible.sync="storeInfoDialogVisible">
+    <!-- 编辑小组详情模态框 -->
+    <el-dialog
+      :title="infoDialogTitle"
+      :visible.sync="storeInfoDialogVisible"
+    >
       <el-form :model="curruntStore">
         <el-form-item
           v-if="infoDialogTitle != '新建小组'"
           label="商店名称"
           :label-width="formLabelWidth"
-
         >
-          <el-input v-model="curruntStore.name" autocomplete="off"></el-input>
+          <el-input
+            v-model="curruntStore.name"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
-        <el-form-item label="商店地址" :label-width="formLabelWidth">
-          <el-input v-model="curruntStore.address" autocomplete="off"></el-input>
+        <el-form-item
+          label="商店地址"
+          :label-width="formLabelWidth"
+        >
+          <el-input
+            v-model="curruntStore.address"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
         <el-form-item
           v-if="infoDialogTitle != '新建小组'"
@@ -90,9 +99,15 @@
           ></el-input>
         </el-form-item>
       </el-form>
-      <div slot="footer" class="dialog-footer">
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
         <el-button @click="storeInfoDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="submitStoreInfo"> 提 交 </el-button>
+        <el-button
+          type="primary"
+          @click="submitStoreInfo"
+        > 提 交 </el-button>
       </div>
     </el-dialog>
 
@@ -129,7 +144,7 @@
         label="操作"
         align="center"
       >
-      <template slot-scope="scope">
+        <template slot-scope="scope">
           <el-button
             @click="editStore(scope.row)"
             type="primary"
@@ -146,8 +161,7 @@
 
       </el-table-column>
     </el-table>
-    
-    
+
     <!-- 分页查询 -->
     <el-pagination
       @size-change="handleSizeChange"
@@ -165,7 +179,6 @@
 <script>
 import shopButton from "../components/shopButton.vue";
 import {
-  getAllStore,
   postStore,
   deleteStore,
   getStoreById,
@@ -180,21 +193,21 @@ export default {
   data() {
     return {
       //每页显示的条数
-      pageSize:5,
+      pageSize: 10,
       //总记录数
-      totalCount:100,
-      storeId:-1,
-      stores:[],
+      totalCount: 100,
+      storeId: -1,
+      stores: [],
       storeMenbers: [],
-      currentStoreId:"",
-      curruntStore:{
-        id:"",
-        name:"",
+      currentStoreId: "",
+      curruntStore: {
+        id: "",
+        name: "",
       },
-      storeMenberDialogVisible:false,
-      storeInfoDialogVisible:false,
-      infoDialogTitle:"",
-      formLabelWidth:"",
+      storeMenberDialogVisible: false,
+      storeInfoDialogVisible: false,
+      infoDialogTitle: "",
+      formLabelWidth: "",
       //表格数据
       tableData: [],
       //复选框选中数据集合
@@ -219,21 +232,24 @@ export default {
     };
   },
   mounted() {
-    this.fetchData(this.currentPage, this.pageSize,this.storeName,this.address);
+    this.fetchData(
+      this.currentPage,
+      this.pageSize,
+      this.storeName,
+      this.address
+    );
   },
-  
-  methods: {
-    fetchData(page, pageSize,storeName,address) {
 
-    getStoreByPage({page, pageSize,storeName,address}).then((res) => {
-      
-      //设置表格数据
-      this.tableData = res.data.data.records;
-      this.searchData = res.data;
-      //设置总记录数
-      this.totalCount = res.data.data.total
-    });
-  },
+  methods: {
+    fetchData(page, pageSize, storeName, address) {
+      getStoreByPage({ page, pageSize, storeName, address }).then((res) => {
+        //设置表格数据
+        this.tableData = res.data.data.records;
+        this.searchData = res.data;
+        //设置总记录数
+        this.totalCount = res.data.data.total;
+      });
+    },
     toggleSelection(rows) {
       if (rows) {
         rows.forEach((row) => {
@@ -251,30 +267,29 @@ export default {
       console.log(`每页 ${val} 条`);
     },
     handleCurrentChange(val) {
-    this.currentPage = val;
-    this.fetchData(val, this.pageSize);
-  },
+      this.currentPage = val;
+      this.fetchData(val, this.pageSize);
+    },
 
     //查询方法
     onSubmit() {
-  const page = 1; // 第一页
-  const pageSize = 5; // 每页显示10条数据
-  const storeName = this.searchData.name; // 从表单中获取店铺名称
-  const address = this.searchData.address; // 从表单中获取地址
-  // 调用分页查询函数
-  getStoreByPage({ page, pageSize, storeName, address }).then((res) => {
-    // 设置表格数据
-    this.tableData = res.data.data.records;
-    // 将获取的数据赋值给一个新的变量
-    const searchData = res.data;
-    // 将新的变量赋值给页面显示的数据变量
-    this.searchData = searchData;
-    // 设置总记录数
-    this.totalCount = res.data.data.total;
-  });
-}
+      const page = 1; // 第一页
+      const pageSize = 10; // 每页显示10条数据
+      const storeName = this.searchData.name; // 从表单中获取店铺名称
+      const address = this.searchData.address; // 从表单中获取地址
+      // 调用分页查询函数
+      getStoreByPage({ page, pageSize, storeName, address }).then((res) => {
+        // 设置表格数据
+        this.tableData = res.data.data.records;
+        // 将获取的数据赋值给一个新的变量
+        const searchData = res.data;
+        // 将新的变量赋值给页面显示的数据变量
+        this.searchData = searchData;
+        // 设置总记录数
+        this.totalCount = res.data.data.total;
+      });
+    },
 
-,
     //提交新数据
     addTable() {
       // console.log(this.form)
@@ -286,41 +301,38 @@ export default {
           this.$message.success(res.data.msg);
           this.dialogVisible = false;
           //关闭窗口
+          location.reload(); // Refresh the current page
         }
       });
     },
     //完成删除
     deleteStore(storeInformation) {
-      this.$confirm("此操作将永久删除小组，是否继续？", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(() => {
-          deleteStore({ ids: storeInformation }).then((res) => {
-            if (res.data.msg == "删除成功") {
-              this.$message({
-                type: "success",
-                message: "删除成功!",
-              });
-              this.tableData = res.data.data;
-            } else throw new Error(res.data.msg);
-          });
-        })
-        .catch(() => {
+  this.$confirm("此操作将永久删除小组，是否继续？", "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+    .then(() => {
+      deleteStore({ ids: storeInformation }).then((res) => {
+        if (res.data.msg == "删除成功") {
           this.$message({
-            type: "info",
-            message: "取消删除",
-          });
-        });
-    },
+            type: "success",
+            message: "删除成功!",
+          });            
+          location.reload(); // reload the current page
+        }
+        else throw new Error(res.data.msg);
+      });            
+    });
+    
+},
     //编辑商店
-    editStore(store){
-    this.infoDialohTitle="小组详情";
-    this.curruntStore = JSON.parse(JSON.stringify(store));
-    this.storeInfoDialogVisible = true ;
+    editStore(store) {
+      this.infoDialohTitle = "小组详情";
+      this.curruntStore = JSON.parse(JSON.stringify(store));
+      this.storeInfoDialogVisible = true;
     },
-    submitStoreInfo(){
+    submitStoreInfo() {
       if (this.infoDialogTitle === "新建小组") {
         postStore({ ...this.curruntStore, storeId: this.tableData })
           .then((res) => {
@@ -340,7 +352,7 @@ export default {
             });
           });
       } else {
-        putStore({ ...this.curruntStore ,})
+        putStore({ ...this.curruntStore })
           .then((res) => {
             this.$message({
               showClose: true,
@@ -356,7 +368,7 @@ export default {
               message: err,
               type: "error",
             });
-           });
+          });
       }
     },
   },
@@ -398,4 +410,5 @@ export default {
   padding: 20px 5px;
 }
 </style> 
+
 
