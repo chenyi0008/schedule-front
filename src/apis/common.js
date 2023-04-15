320dae2498561c735b35e2da21e1c332d7f78208
LunaSeki <LunaSeki@outlook.com>
2023-04-15 17:56:46 +0800

    fixed(upload csv): fixed upload function

--- src/apis/common.js
+++ src/apis/common.js
@@ -1,10 +1,13 @@
 import request from "@/utils/request";
+import axios from "axios";
 
-export function postSchedule(storeId) {
-  return request({
-    url: "schedule",
-    method: "post",
-    storeId,
-  });
+export function postSchedule(storeId, file) {
+	return request({
+		url: `common/uploadCSV?storeId=${storeId}`,
+		method: "post",
+		headers: {
+			"Content-Type": "multipart/form-data",
+		},
+		data: file,
+	});
 }
-
